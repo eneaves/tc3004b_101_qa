2c30b0e448fbd4b064e35bf1860260f78583fef2
Emiliano Neaves <emilianoneaves@MacBook-Air-de-Emiliano-49.local>
2025-03-06 08:47:50 -0600

    restas test

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-const res = require("express/lib/response");
-
-function calculate(){
-    const num1 = document.getElementById('num1').value;
-    const num2 = document.getElementById('num2').value;
-
-    let result;
-    result = parseInt(num1) + parseInt(num2);
-
-    document.getElementById('result').innerText = "Result: " + result;
-}
-
-function resetFields(){
+function add() {
+    const num1 = parseInt(document.getElementById('num1').value) || 0;
+    const num2 = parseInt(document.getElementById('num2').value) || 0;
+  
+    const result = num1 + num2;
+    document.getElementById('result').innerText = 'Result: ' + result;
+  }
+  
+  function subtract() {
+    const num1 = parseInt(document.getElementById('num1').value) || 0;
+    const num2 = parseInt(document.getElementById('num2').value) || 0;
+  
+    const result = num1 - num2;
+    document.getElementById('result').innerText = 'Result: ' + result;
+  }
+  
+  function resetFields() {
     document.getElementById('num1').value = '';
     document.getElementById('num2').value = '';
-    document.getElementById('result').innerText = '';
-}
+    document.getElementById('result').innerText = 'Result:';
+  }
+  
