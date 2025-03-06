12d0e628ad6708deb6965938bb0f94cf34a88428
Emiliano Neaves <115823092+eneaves@users.noreply.github.com>
2025-03-06 08:08:30 -0600

    Update floatingpointprecision.spec.js

--- tests/floatingpointprecision.spec.js
+++ tests/floatingpointprecision.spec.js
@@ -71,7 +71,7 @@ describe('floatingpointprecision', function() {
     });
 
   it('floatingpointprecision', async function() {
-    await driver.get("http://localhost::8000/")
+    await driver.get("http://localhost:8000/")
     await driver.manage().window().setRect({ width: 1470, height: 920 })
     await driver.findElement(By.id("num1")).click()
     await driver.findElement(By.id("num1")).sendKeys("0.1")
